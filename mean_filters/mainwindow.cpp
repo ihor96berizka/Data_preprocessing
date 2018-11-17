305650fd3e634e3eb8996f7376f2c91585d7c344
Ihor Berizka <ihor.berizka@gmail.com>
2018-11-17 22:29:12 +0200

    Implemented gui

--- mean_filters/mainwindow.cpp
+++ mean_filters/mainwindow.cpp
@@ -6,9 +6,73 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    createActions();
+    createMenus();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::createActions()
+{
+    genDiscreteNumSignal = new QAction(tr("Discrete signal"), this);
+    connect(genDiscreteNumSignal, &QAction::triggered, this, &MainWindow::genDiscreteNumSignalSlot);
+
+    genSinSignal = new QAction(tr("Sin signal"), this);
+    connect(genSinSignal, &QAction::triggered, this, &MainWindow::genSinSignalSlot);
+
+    newMeanFilter = new QAction(tr("New mean"), this);
+    connect(newMeanFilter, &QAction::triggered, this, &MainWindow::newMeanFilterSlot);
+
+    movingMeanFilter = new QAction(tr("Moving mean"), this);
+    connect(movingMeanFilter, &QAction::triggered, this, &MainWindow::movingMeanFilterSlot);
+
+    expSmoothFilter = new QAction(tr("Exp smoothing"), this);
+    connect(expSmoothFilter, &QAction::triggered, this, &MainWindow::expSmoothFilterSlot);
+
+    clearAllData = new QAction(tr("Clear all data"), this);
+    connect(clearAllData, &QAction::triggered, this, &MainWindow::clearAllDataSlot);
+}
+
+void MainWindow::createMenus()
+{
+    signalMenu = menuBar()->addMenu(tr("Signals"));
+    signalMenu->addActions({genDiscreteNumSignal, genSinSignal, clearAllData});
+
+    filterMenu = menuBar()->addMenu(tr("Filters"));
+
+    meanFilters = filterMenu->addMenu(tr("Mean filters"));
+    meanFilters->addActions({newMeanFilter, movingMeanFilter, expSmoothFilter});
+}
+
+void MainWindow::genDiscreteNumSignalSlot()
+{
+
+}
+
+void MainWindow::genSinSignalSlot()
+{
+
+}
+
+void MainWindow::newMeanFilterSlot()
+{
+
+}
+
+void MainWindow::movingMeanFilterSlot()
+{
+
+}
+
+void MainWindow::expSmoothFilterSlot()
+{
+
+}
+
+void MainWindow::clearAllDataSlot()
+{
+
 }
--- mean_filters/mainwindow.h
+++ mean_filters/mainwindow.h
@@ -2,7 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAction>
+#include <QMenu>
 
+#include <QtCharts/QChartView>
+#include <QtCharts/QChart>
+#include <QtCharts/QScatterSeries>
+
+
+using namespace QtCharts;
 namespace Ui {
 class MainWindow;
 }
@@ -17,6 +25,37 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    //menu and buttons
+    QMenu* signalMenu;
+    QMenu* filterMenu;
+    QMenu* meanFilters;
+
+    QAction* genDiscreteNumSignal;
+    QAction* genSinSignal;
+
+    QAction* newMeanFilter;
+    QAction* movingMeanFilter;
+    QAction* expSmoothFilter;
+    QAction* clearAllData;
+
+    //chart routine
+    QChartView* chartView;
+    QChart* chart;
+    QScatterSeries* realSignalSeries;
+    QScatterSeries* filteredSignalSeries;
+    QScatterSeries* idealSignalSeries;
+
+
+
+    void createActions();
+    void createMenus();
+private slots:
+    void genDiscreteNumSignalSlot();
+    void genSinSignalSlot();
+    void newMeanFilterSlot();
+    void movingMeanFilterSlot();
+    void expSmoothFilterSlot();
+    void clearAllDataSlot();
 };
 
 #endif // MAINWINDOW_H
